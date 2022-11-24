c3f99faeec87149becdb82d84f9cc788a394dc76
BrightX0 <augustinobright@gmail.com>
2022-11-24 11:05:25 +0300

    ignore changes

--- Contract/Voting.sol
+++ Contract/Voting.sol
@@ -45,4 +45,31 @@ contract Balot {
         require(voters[voter].weight == 0);
         voters[voter].weight = 1;
     }
+
+    function delegate(address to) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "You have no right to vote");
+        require(!sender.voted, "You already voted.");
+
+        require(to != msg.sender, "Self-delegation is disallowed.");
+
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+
+            require(to != msg.sender, "Found loop in delegation.");
+        }
+
+        Voter storage delegate_ = voters[to];
+
+        require(delegate_.weight >= 1);
+
+        sender.voted = true;
+        sender.delegate = to;
+
+        if (delegate_.voted) {
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            delegate_.weight += sender.weight;
+        }
+    }
 }
